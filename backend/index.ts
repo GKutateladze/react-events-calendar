2308e53515e7a69deb973f7a8e59cdfbac000482
gkutateladze <georgy.kutateladze@yandex.ru>
2020-12-29 13:54:31 +0300

    Connected project to MongoDB

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
 
+const dbURI = 'mongodb+srv://george:test12345@eventscalendar.85svm.mongodb.net/events?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
+  })
+  .catch((error: string) => console.log(error));
 
-app.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}`);
-});
+app.use(cors());
