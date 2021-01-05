1df0706d4c921a66959029696d27b1fdf6fddfc4
gkutateladze <georgy.kutateladze@yandex.ru>
2021-01-05 17:51:15 +0300

    Styles

--- src/components/atoms/EventForm/EventForm.tsx
+++ src/components/atoms/EventForm/EventForm.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setCounter } from '../../../_store/actions/events.actions';
 import { Button } from 'root-front';
 import { IStore } from '../../../_store';
+import moment from 'moment';
 
 interface IProps {
 }
@@ -13,6 +14,7 @@ const EventForm: React.FC<IProps> = () => {
   const dispatch = useDispatch();
 
   const counter: number = useSelector((store: IStore) => store.events.counter);
+  const date = moment().add(counter, 'day').format('DD.MM.YYYY')
 
   // -------------------------------------------------------------------------------------------------------------------
 
@@ -24,6 +26,7 @@ const EventForm: React.FC<IProps> = () => {
 
   return (
     <div className='event-form'>
+        <p className='event-form__date'>{date}</p>
         <Button className='event-form__button' size='small' onClick={ () => handleCounter(-1) }>Previous</Button>
         <Button className='event-form__button' size='small' onClick={ () => handleCounter(1) }>Next</Button>
         <Button className='event-form__button' size='small' onClick={ () => handleCounter(0) } disabled={counter === 0}>Today</Button>
